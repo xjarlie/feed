63f9e06010963ad697cf927f78618f7cf8ce9817
Charlie Appleton <xjarlie@gmail.com>
2023-01-31 21:04:57 +0900

    add prisma + sqlite

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
